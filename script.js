c5c2eb0fa14bf402b27acc5b0ae9371b26f1eb19
desirekataku <138031131+desirekataku@users.noreply.github.com>
2025-01-20 17:50:19 +0100

    les 3 fichier

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Merci pour votre message, je vous répondrai bientôt !');
+    this.reset();
+});
